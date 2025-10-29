51f116789e3e876b6561ab7730e75b5c6169a552
D Vivek Pai <vivekpai2005810@gmail.com>
2025-10-29 11:11:31 +0530

    trying to fix deployement

--- backend/utils/reminderService.js
+++ backend/utils/reminderService.js
@@ -153,6 +153,7 @@ const sendEmail = async (to, subject, text) => {
 
     await transporter.sendMail(mailOptions);
     console.log(`✅ Email sent to ${to}`);
+    return true;
   } catch (error) {
     console.error("❌ Error sending email:", error.message);
     throw new Error("Failed to send email");
